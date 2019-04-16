50c84c01c0722f96f9bfa07d613bed377c4951be
Beau Johnson <beau.k.johnson@gmail.com>
2019-04-15 20:12:03 -0700

    updates 4/15

---
+++ app/src/main/java/com/example/firebasetest/Activities/EnterSHActivity.java
@@ -0,0 +1,13 @@
+package com.example.firebasetest.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterSHActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_sh);
+    }
+}
